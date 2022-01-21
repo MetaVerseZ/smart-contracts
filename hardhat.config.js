6fc7af8eb04056b713fdbbf38531307ec573c4cc
Nadhir <nadhirxz@gmail.com>
2022-01-22 00:25:33 +0100

    Contract verification for bscscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require('@nomiclabs/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
+require('@nomiclabs/hardhat-etherscan');
+
+const { bscApi } = require('./secret.json');
 
 module.exports = {
 	defaultNetwork: 'ganache',
@@ -7,14 +10,15 @@ module.exports = {
 		ganache: {
 			url: 'http://127.0.0.1:7545',
 		},
+		bsc: {
+			url: 'https://bsc-dataseed.binance.org/',
+			chainId: 56,
+		},
 	},
 	solidity: {
 		version: '0.8.9',
-		settings: {
-			optimizer: {
-				enabled: true,
-				runs: 200,
-			},
-		},
+	},
+	etherscan: {
+		apiKey: bscApi,
 	},
 };
