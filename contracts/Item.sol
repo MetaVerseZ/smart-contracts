20f7bb28f7ff6229881a14e6a06d92ca2661627b
Nadhir <nadhirxz@gmail.com>
2022-04-04 03:21:17 +0100

    Recreated item contract

--- contracts/Item.sol
+++ contracts/Item.sol
@@ -1,42 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 
-contract Item is ERC721URIStorage {
-	uint256 public _tokenId;
+contract Item is ERC1155 {
+	uint256 public _tokenId = 0;
 
-	address public market = address(0);
-	address public admin;
+	address public _market = address(0);
+	address[] public _admins;
 
-	event Minted(address owner, uint256 tokenId);
+	mapping(uint256 => string) private _tokenURIs;
 
-	constructor(address _adminAddress) ERC721('Meta Z Item', 'MZI') {
-		admin = _adminAddress;
+	event Minted(address owner, uint256 tokenId, uint256 amount);
+
+	constructor() ERC1155('Meta Z Item') {
+		_admins = [msg.sender];
 	}
 
-	function _baseURI() internal pure override returns (string memory) {
-		return 'ipfs://';
+	function setMarket(address market) public {
+		require(isAdmin(msg.sender), 'not admin');
+		_market = market;
+		setApprovalForAll(_market, true);
 	}
 
-	function setMarket(address _marketAddress) public {
-		require(msg.sender == admin, 'not admin');
-		market = _marketAddress;
-	}
-
-	function mint(string memory uri) public {
-		require(market != address(0), 'zero address');
-		uint256 tokenId = _tokenId;
-		_safeMint(msg.sender, tokenId);
-		_setTokenURI(tokenId, uri);
-		setApprovalForAll(market, true);
-		emit Minted(msg.sender, tokenId);
+	function mint(string memory uri, uint256 amount) public {
+		require(_market != address(0), 'market undefined');
+		_mint(msg.sender, _tokenId, amount, '');
+		_setTokenURI(_tokenId, uri);
+		setApprovalForAll(_market, true);
+		emit Minted(msg.sender, _tokenId, amount);
 		_tokenId++;
 	}
 
-	function burn(uint256 tokenId) public {
-		require(ERC721.ownerOf(tokenId) == msg.sender, 'only owner can burn');
-		super._burn(tokenId);
+	function tokenURI(uint256 tokenId) public view returns (string memory) {
+		return _tokenURIs[tokenId];
+	}
+
+	function _setTokenURI(uint256 tokenId, string memory uri) private {
+		_tokenURIs[tokenId] = uri;
+	}
+
+	function isAdmin(address account) public view returns (bool) {
+		bool admin = false;
+		for (uint256 i = 0; i < _admins.length; i++) {
+			if (_admins[i] == account) {
+				admin = true;
+				break;
+			}
+		}
+		return admin;
+	}
+
+	function setAdmins(address[] memory admins) public {
+		require(isAdmin(msg.sender), 'admin only');
+		_admins = admins;
 	}
 }
---
+++ test/partials/item.test.js
@@ -0,0 +1,48 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+// const { create, urlSource } = require('ipfs-http-client');
+
+const test = l => {
+	const length = l ?? 2;
+	return () => {
+		const testItemPrice = ethers.utils.parseEther('100');
+
+		// const ipfs = create({ silent: true });
+		// const gateway = 'http://ipfs.io/ipfs';
+
+		it('mint and list items', async () => {
+			await Promise.all(
+				Array.from({ length }, (v, k) => k).map(async id => {
+					// const metadata = {
+					// 	id,
+					// 	name: `item ${id}`,
+					// 	description: 'some description',
+					// 	image: (await ipfs.add(urlSource('https://picsum.photos/64'))).cid.toString(),
+					// };
+
+					// const { cid } = await ipfs.add(JSON.stringify(metadata));
+					// const uri = `${gateway}/${cid}`;
+
+					const amount = Math.ceil(Math.random() * 100);
+
+					await item.mint(id.toString(), amount);
+					expect(await item.tokenURI(id)).to.equal(id.toString());
+
+					await market.listERC1155(item.address, id, testItemPrice, amount);
+
+					const listing = await market.getListing(id);
+
+					expect(listing.sellers).to.include(main.address);
+
+					const index = listing.sellers.indexOf(main.address);
+					expect(ethers.utils.formatUnits(listing.amounts[index], 0)).to.equal(amount.toString());
+				})
+			);
+
+			const listings = await market.listings(item.address);
+			expect(listings.length).to.equal(length);
+		});
+	};
+};
+
+module.exports = test;
